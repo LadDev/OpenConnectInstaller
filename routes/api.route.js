c66b99b3db0dae8716cd905ba9744f35b18f757d
Lad Ab <laddev.site@inbox.lv>
2023-05-23 21:09:27 +0300

    Add test web api

--- routes/api.route.js
+++ routes/api.route.js
@@ -16,6 +16,7 @@ router.get("/show/status", async (req, res) => {
             }
         });
     } catch (error) {
+        console.error(error)
         res.status(500).json({code: -1, message: "Something went wrong, please try again"})
     }
 
