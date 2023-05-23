1df0ebcc166b59658e6794f2f33b2d572fa33e92
Lad Ab <laddev.site@inbox.lv>
2023-05-23 18:27:01 +0300

    Add test web api

--- routes/api.route.js
+++ routes/api.route.js
@@ -152,112 +152,21 @@ router.get("/show/users", async (req, res) => {
         command.stdout.on('data', (data) => {
             const tmpDataArray = data.toString().replace("\t", "").split("\n");
 
-            console.log(tmpDataArray)
-
             const usersArr = []
 
             const headers = tmpDataArray[0].split(/\s+/);
             const values = tmpDataArray[1].split(/\s+/);
 
-
-
             for(const usrLine of tmpDataArray.slice(1)){
                 const user = {};
                 headers.forEach((header, index) => {
                     if(header.trim() !== "") {
-                        user[header.trim()] = values[index].trim();
+                        user[header.trim().replace("-","_")] = values[index].trim();
                     }
                 });
 
                 usersArr.push(user)
             }
-
-
-            // for (const d of tmpDataArray) {
-            //     const arrD = d.split(":")
-            //     if (arrD.length >= 2) {
-            //         const title = arrD[0].replace("\t", "").trim()
-            //         let value = arrD[1].replace("\t", "").trim()
-            //
-            //         if(arrD.length>2){
-            //             const newArr = arrD.slice(1);
-            //             value=newArr.join(":")
-            //         }
-            //
-            //         switch (title.trim()) {
-            //             case "Status":
-            //                 dataObj.general_info.status = value
-            //                 break;
-            //             case "Server PID":
-            //                 dataObj.general_info.server_pid = Number(value)
-            //                 break;
-            //             case "Sec-mod PID":
-            //                 dataObj.general_info.sec_mod_pid = Number(value)
-            //                 break;
-            //             case "Up since":
-            //                 dataObj.general_info.up_since = Number(value)
-            //                 break;
-            //             case "Active sessions":
-            //                 dataObj.general_info.active_sessions = Number(value)
-            //                 break;
-            //             case "Total sessions":
-            //                 dataObj.general_info.total_sessions = Number(value)
-            //                 break;
-            //             case "Total authentication failures":
-            //                 dataObj.general_info.total_authentication_failures = Number(value)
-            //                 break;
-            //             case "IPs in ban list":
-            //                 dataObj.general_info.ips_in_ban_list = Number(value)
-            //                 break;
-            //
-            //             case "Last stats reset":
-            //                 dataObj.current_stats_info.last_stats_reset = value
-            //                 break;
-            //             case "Sessions handled":
-            //                 dataObj.current_stats_info.sessions_handled = value
-            //                 break;
-            //             case "Timed out sessions":
-            //                 dataObj.current_stats_info.timed_out_sessions = value
-            //                 break;
-            //             case "Timed out (idle) sessions":
-            //                 dataObj.current_stats_info.timed_out_idle_sessions = value
-            //                 break;
-            //             case "Closed due to error sessions":
-            //                 dataObj.current_stats_info.closed_due_to_error_sessions = value
-            //                 break;
-            //             case "Authentication failures":
-            //                 dataObj.current_stats_info.authentication_failures = value
-            //                 break;
-            //             case "Average auth time":
-            //                 dataObj.current_stats_info.average_auth_time = value
-            //                 break;
-            //             case "Max auth time":
-            //                 dataObj.current_stats_info.max_auth_time = value
-            //                 break;
-            //             case "Average session time":
-            //                 dataObj.current_stats_info.average_session_time = value
-            //                 break;
-            //             case "Max session time":
-            //                 dataObj.current_stats_info.max_session_time = value
-            //                 break;
-            //             case "Min MTU":
-            //                 dataObj.current_stats_info.min_mtu = value
-            //                 break;
-            //             case "Max MTU":
-            //                 dataObj.current_stats_info.max_mtu = value
-            //                 break;
-            //             case "RX":
-            //                 dataObj.current_stats_info.rx = value
-            //                 break;
-            //             case "TX":
-            //                 dataObj.current_stats_info.tx = value
-            //                 break;
-            //             default:
-            //                 break;
-            //         }
-            //
-            //     }
-            // }
 
             res.status(200).json({code: 0, users: usersArr})
         });
