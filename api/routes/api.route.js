9ee4b9e7bc646418023986c5a331fa80fc4831db
Lad Ab <laddev.site@inbox.lv>
2023-05-24 23:01:34 +0300

    Init APP

--- api/routes/api.route.js
+++ api/routes/api.route.js
@@ -40,7 +40,7 @@ router.post("/auth", async (req, res) => {
         const {email, password} = req.body
 
         if(password === authData.password && email === authData.email){
-            const clientIP = req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
+            //const clientIP = req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
 
             let token = jwt.sign({email: email, ip:clientIP}, authData.salt, {expiresIn: "1d"})
 
--- test_config.sh
+++ test_config.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#PASSGEN=$(openssl rand -base64 12)
-#SALT=$(openssl rand -base64 48)
-#
-#EMAIL="service.ru@mail.ru"
-#
-#echo $SALT
-#echo $PASSGEN
-#
-#sudo tee api/config.json > /dev/null <<EOF
-#{
-#  "email":"$EMAIL",
-#  "password":"$PASSGEN",
-#  "salt":"$SALT"
-#}
-#EOF
+PASSGEN=$(openssl rand -base64 12)
+SALT=$(openssl rand -base64 48)
 
-cd app && yarn build
-cd ..
-cp -r app/build/* test/
+EMAIL="service.ru@mail.ru"
+
+echo $SALT
+echo $PASSGEN
+
+sudo tee api/config.json > /dev/null <<EOF
+{
+  "email":"$EMAIL",
+  "password":"$PASSGEN",
+  "salt":"$SALT"
+}
+EOF
+
+#cd app && yarn build
+#cd ..
+#cp -r app/build/* test/
